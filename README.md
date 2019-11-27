cb1a0bc4800620f031deff9bcd5298a0ac8eebf0
John Chase <ratteal@gmail.com>
2019-11-27 15:34:22 +0000

    Initial project setup.

---
+++ README.md
@@ -0,0 +1,6 @@
+# sync-gandi-dns
+
+Simple Python script to keep the IP address records for a domain registered with [Gandi](https://www.gandi.net) up to
+date with the dynamic values assigned by your ISP. 
+
+This includes resource records for both IPV4 (`A`) and IPV6 (`AAAA`).
---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='syncgandidns',
+    version='0.0.1',
+    description='Keep .',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='John Chase',
+    author_email='ratteal@gmail.com',
+    python_requires='>=3.5.3',
+    url='https://github.com/RatJuggler/sync-gandi-dns',
+    packages=find_packages(exclude=['tests']),
+    entry_points={
+        'console_scripts': [
+            'syncgandidns = syncgandidns.__main__:syncgandidns',
+        ]
+    },
+    install_requires=[
+        # Check latest releases on piwheels: https://www.piwheels.hostedpi.com/
+        'click ==7.0',
+        'requests ==2.21.0'
+    ],
+    test_suite='tests',
+    tests_require=[
+        'coverage',
+        'docutils',
+        'flake8',
+        'mock',
+        'testfixtures'
+    ],
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 1 - Planning',
+        'Programming Language :: Python :: 3.5',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: Internet :: Name Service (DNS)'
+    ]
+)
