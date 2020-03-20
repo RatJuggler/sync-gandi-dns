0cb02c59bf1b323291039b50cd2f79e2be4ebef3
John Chase <ratteal@gmail.com>
2020-03-20 09:48:36 +0000

    Start option to disable IPV6 update.

--- syncgandidns/__main__.py
+++ syncgandidns/__main__.py
@@ -13,24 +13,27 @@ IPV6_ADDRESS = IPv6AddressParamType()
 
 @click.command(help='''
     Sync a dynamic IP address (V4 & V6) with a Gandi DNS domain entry.\n
-    The external IP address is determined automatically.\n 
+    The external IP address is determined automatically by default.\n 
     DOMAIN: The domain to update the DNS for.\n
     APIKEY: Your Gandi API key.
     ''')
 @click.version_option()
 @click.argument('domain', nargs=1, type=click.STRING, required=True)
 @click.argument('apikey', nargs=1, type=click.STRING, required=True)
-@click.option('-ipv4', 'ipv4', type=IPV4_ADDRESS,
+@click.option('-no-ipv6', '--no-ipv6-update', 'no_ipv6', is_flag=True,
+              help='Don\'t update the IPV6 address, will override \'-ipv4\'')
+@click.option('-ipv4', '--ipv4-address', 'ipv4', type=IPV4_ADDRESS,
               help='Override the IPV4 address to update the domain DNS with.')
-@click.option('-ipv6', 'ipv6', type=IPV6_ADDRESS,
+@click.option('-ipv6', '--ipv6-address', 'ipv6', type=IPV6_ADDRESS,
               help='Override the IPV6 address to update the domain DNS with.')
 @click.option('-l', '--log-level', 'level', type=click.Choice(['DEBUG', 'VERBOSE', 'INFO', 'WARNING']),
               help='Show additional logging information.', default='INFO', show_default=True)
-def syncgandidns(domain: str, apikey: str, ipv4: str, ipv6: str, level: str) -> None:
+def syncgandidns(domain: str, apikey: str, no_ipv6: bool, ipv4: str, ipv6: str, level: str) -> None:
     """
     Sync local dynamic IP address with Gandi DNS.
     :param domain: To sync IP address for
     :param apikey: To access the API with
+    :param no_ipv6: Don't update IPV6
     :param ipv4: To sync to the domain DNS
     :param ipv6: To sync to the domain DNS
     :param level: Set a logging level; DEBUG, VERBOSE, INFO or WARNING
