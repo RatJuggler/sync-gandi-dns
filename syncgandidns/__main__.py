216cfd4ff106ebbaab4eaf681d63111b257f096f
John Chase <ratteal@gmail.com>
2021-02-02 19:04:13 +0000

    Should be options to not update IPV4 and/or IPV6.

--- syncgandidns/__main__.py
+++ syncgandidns/__main__.py
@@ -25,24 +25,27 @@ def test_access(domain: str, apikey: str) -> None:
               help='The domain to update the DNS for.')
 @click.option('-a', '--apikey', 'apikey', type=click.STRING, required=True, envvar='GANDI_APIKEY',
               help='Your Gandi API key. Taken from environment variable GANDI_APIKEY if present.')
-@click.option('-no-ipv6', '--no-ipv6-update', 'no_ipv6', is_flag=True,
-              help='Don\'t update the IPV6 address, will override \'-ipv6\'.')
 @click.option('-ipv4', '--ipv4-address', 'ipv4', type=IPV4_ADDRESS,
               help='Override the IPV4 address to update the domain DNS with.')
+@click.option('-no-ipv4', '--no-ipv4-update', 'no_ipv4', is_flag=True,
+              help='Don\'t update the IPV4 address, will override \'-ipv4\'.')
 @click.option('-ipv6', '--ipv6-address', 'ipv6', type=IPV6_ADDRESS,
               help='Override the IPV6 address to update the domain DNS with.')
+@click.option('-no-ipv6', '--no-ipv6-update', 'no_ipv6', is_flag=True,
+              help='Don\'t update the IPV6 address, will override \'-ipv6\'.')
 @click.option('-l', '--log-level', 'level', type=click.Choice(['DEBUG', 'VERBOSE', 'INFO', 'WARNING']),
               help='Show additional logging information.', default='INFO', show_default=True)
 @click.option('-t', '--test', 'test', default=False, is_flag=True,
               help="Test the Gandi API key and exit.", show_default=True)
-def syncgandidns(domain: str, apikey: str, no_ipv6: bool, ipv4: str, ipv6: str, level: str, test: bool) -> None:
+def syncgandidns(domain: str, apikey: str, ipv4: str, no_ipv4: bool, ipv6: str, no_ipv6: bool, level: str, test: bool) -> None:
     """
     Sync local dynamic IP address with Gandi DNS.
     :param domain: To sync IP address for
     :param apikey: To access the API with
-    :param no_ipv6: Don't update IPV6
     :param ipv4: To sync to the domain DNS
+    :param no_ipv4: Don't update IPV4
     :param ipv6: To sync to the domain DNS
+    :param no_ipv6: Don't update IPV6
     :param level: Set a logging level; DEBUG, VERBOSE, INFO or WARNING
     :param test: Test access to a domains records
     :return: No meaningful return
@@ -52,7 +55,7 @@ def syncgandidns(domain: str, apikey: str, no_ipv6: bool, ipv4: str, ipv6: str, 
     if test:
         test_access(domain, apikey)
     else:
-        do_sync(domain, apikey, no_ipv6, ipv4, ipv6)
+        do_sync(domain, apikey, no_ipv4, ipv4, no_ipv6, ipv6)
 
 
 if __name__ == '__main__':
