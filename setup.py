e9a4fbfd56666901e9b135bc089ee40cade78127
John Chase <ratteal@gmail.com>
2021-02-02 08:58:42 +0000

    Bump version and use latest dependencies.

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setup(
     name='syncgandidns',
-    version='0.0.2',
+    version='0.0.3',
     description='Sync a dynamic IP address with a Gandi DNS domain entry.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     keywords='gandi dns',
     author='John Chase',
     author_email='ratteal@gmail.com',
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     url='https://github.com/RatJuggler/sync-gandi-dns',
     project_urls={
         "Documentation": "https://github.com/RatJuggler/sync-gandi-dns",
@@ -26,9 +26,9 @@ setup(
         ]
     },
     install_requires=[
-        # Check latest releases on piwheels: https://www.piwheels.hostedpi.com/
-        'click ==7.1.1',
-        'requests ==2.23.0'
+        # Check latest releases on piwheels: https://www.piwheels.org/
+        'click ==7.1.2',
+        'requests ==2.25.1'
     ],
     test_suite='tests',
     tests_require=[
@@ -43,7 +43,7 @@ setup(
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
         'License :: OSI Approved :: MIT License',
         'Development Status :: 4 - Beta',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Operating System :: POSIX :: Linux',
         'Topic :: Internet :: Name Service (DNS)'
     ]
