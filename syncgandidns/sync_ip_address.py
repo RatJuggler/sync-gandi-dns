d15bf31c2a51fc8972c7d62ac55c2df5fd4fb0d7
John Chase <ratteal@gmail.com>
2021-02-02 19:05:01 +0000

    Should be options to not update IPV4 and/or IPV6.

--- syncgandidns/sync_ip_address.py
+++ syncgandidns/sync_ip_address.py
@@ -19,7 +19,7 @@ def _sync_ip(ip_type: str, new_ip: Optional[str], get_ip: callable, update_ip: c
         logging.info("{0} updated to: {1}".format(ip_type, new_ip))
 
 
-def _sync_ip_address(domain: str, ipv4: Optional[str], ipv6: Optional[str], apikey: str) -> None:
+def _sync_ip_address(apikey: str, domain: str, ipv4: Optional[str], ipv6: Optional[str]) -> None:
     gandi_api = GandiAPI(apikey, domain)
     _sync_ip('IPV4', ipv4, gandi_api.get_ipv4_address, gandi_api.update_ipv4_address)
     _sync_ip('IPV6', ipv6, gandi_api.get_ipv6_address, gandi_api.update_ipv6_address)
@@ -34,12 +34,12 @@ def _get_ip_address(ip_type: str, get_ip: callable, ip_validate: callable) -> Op
     return ip_address
 
 
-def do_sync(domain: str, apikey: str, no_ipv6: bool, ipv4: str, ipv6: str) -> None:
+def do_sync(domain: str, apikey: str, no_ipv4: bool, ipv4: str, no_ipv6: bool, ipv6: str) -> None:
     logging.info("Updating DNS for domain: {0}".format(domain))
-    logging.info("Update IPV4 to: {0}".format('<automatic lookup>' if ipv4 is None else ipv4))
-    if ipv4 is None:
+    logging.info("Update IPV4 to: {0}".format('<disabled>' if no_ipv4 else '<automatic lookup>' if ipv4 is None else ipv4))
+    if not no_ipv4 and ipv4 is None:
         ipv4 = _get_ip_address('IPV4', get_ipv4_address, IPV4_ADDRESS.validate)
     logging.info("Update IPV6 to: {0}".format('<disabled>' if no_ipv6 else '<automatic lookup>' if ipv6 is None else ipv6))
     if not no_ipv6 and ipv6 is None:
         ipv6 = _get_ip_address('IPV6', get_ipv6_address, IPV6_ADDRESS.validate)
-    _sync_ip_address(domain, ipv4, ipv6, apikey)
+    _sync_ip_address(apikey, domain, ipv4, ipv6)
