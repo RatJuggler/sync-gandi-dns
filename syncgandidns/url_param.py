e104b594b62c01191f39959eb5a72a1ba15d0b5f
John Chase <ratteal@gmail.com>
2021-03-01 11:46:34 +0000

    Align comments.

--- syncgandidns/url_param.py
+++ syncgandidns/url_param.py
@@ -4,11 +4,11 @@ from click import ParamType
 from typing import Optional
 
 pattern = re.compile(
-    r'^https?://'   # http:// or https://
-    r'(?:(?:[A-Z0-9](?:[A-Z0-9-_]{0,61}[A-Z0-9])?\.)+[A-Z0-9][A-Z0-9-_]{0,61}[A-Z]|'    # see domain validation
-    r'localhost|'   # or localhost
-    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # or ip address
-    r'(?::\d+)?$',    # optional port
+    r'^https?://'  # http:// or https://
+    r'(?:(?:[A-Z0-9](?:[A-Z0-9-_]{0,61}[A-Z0-9])?\.)+[A-Z0-9][A-Z0-9-_]{0,61}[A-Z]|'  # see domain validation
+    r'localhost|'  # or localhost
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # or ip address
+    r'(?::\d+)?$',  # optional port
     re.IGNORECASE
 )
 
